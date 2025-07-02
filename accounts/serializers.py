827bca2628631105b66b8ead53a39db957fc7f19
niccolomagnolfi <niccomagno@icloud.com>
2025-07-02 16:45:04 +0200

    aggiunta registrazione utente

---
+++ accounts/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import CustomUser
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'password', 'email', 'bio')
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            password=validated_data['password'],
+            bio=validated_data.get('bio', '')
+        )
+        return user
